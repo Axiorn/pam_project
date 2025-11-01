92840de19a368d1abd9056f63669961c276f353b
latief <80023698+Axiorn@users.noreply.github.com>
2025-11-01 18:30:27 +0700

    remove unuseable route

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import '../../../routes/app_routes.dart';
 import 'bottom_navbar.dart';
 import '../profile/profile_screen.dart';
 import '../feedback/feedback_screen.dart';
