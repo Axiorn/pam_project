653acdcc97ffec4bd8f7f534c8a94bd3a402adfa
latief <80023698+Axiorn@users.noreply.github.com>
2025-11-02 20:51:33 +0700

    make func notif

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,39 @@
 // ignore_for_file: dead_code
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:pam_project/core/services/hive_view.dart'; // melihat seluruh data hive
 import 'package:path_provider/path_provider.dart';
-import 'package:pam_project/presentation/screens/subscription/subscription_screen.dart';
+import 'package:awesome_notifications/awesome_notifications.dart';
 
 import 'core/constants/hive_boxes.dart';
 import 'core/models/user_model.dart';
 import 'core/models/bmi_result_model.dart';
-import 'routes/app_routes.dart';
+import 'core/services/hive_view.dart';
+import 'presentation/screens/subscription/subscription_screen.dart';
 import 'presentation/screens/auth/login_screen.dart';
 import 'presentation/screens/auth/register_screen.dart';
-import 'presentation/themes/app_theme.dart';
 import 'presentation/screens/home/home_screen.dart';
 import 'presentation/screens/bmi/bmi_history_screen.dart';
+import 'presentation/themes/app_theme.dart';
+import 'routes/app_routes.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  
+  await AwesomeNotifications().initialize(
+    null,
+    [
+      NotificationChannel(
+        channelKey: 'alerts',
+        channelName: 'Alerts',
+        channelDescription: 'Notifikasi untuk BMI dan langganan',
+        defaultColor: Colors.deepPurple,
+        ledColor: Colors.deepPurple,
+        importance: NotificationImportance.High,
+        channelShowBadge: true,
+      )
+    ],
+    debug: true,
+  );
 
   final dir = await getApplicationDocumentsDirectory();
   await Hive.initFlutter(dir.path);
